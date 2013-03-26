ec25ea678d334cf643f34bcc774205421318e1d9
José manuel Barroso Galindo <theypsilon@gmail.com>
2013-03-26 13:59:35 +0100

    Fixing initial things

--- run.sh
+++ run.sh
@@ -1,3 +1,29 @@
 #/bin/sh
 
-./lib/moai-dev/cmake/moai/moai src/main.lua
+echo ''
+echo '## starting itemology! ##'
+echo ''
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+runThisAndExit()
+{
+	if command_exists $1 ; then
+		echo ''
+		echo '#  GO!!'
+		echo ''
+		$1 src/main.lua;
+		exit 1; 
+	else
+		echo '! "'$1'" not present in the system'
+	fi
+}
+
+runThisAndExit './lib/moai-dev/cmake/moai/moai'
+runThisAndExit 'moai'
+
+echo ''
+echo '## cant initialize itemology, cause moai was not found! ##'
+echo ''
