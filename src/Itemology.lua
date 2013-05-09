f0bbf033094349bd76cc5cc2b8a139553f3d41a5
José manuel Barroso Galindo <theypsilon@gmail.com>
2013-05-09 12:57:40 +0200

    Ident changes

--- src/Itemology.lua
+++ src/Itemology.lua
@@ -4,16 +4,18 @@ require 'Includes'
 tiled.path = "res/maps/"
 
 scale = 2
-	if scale then
-		local oldGetWidth  = love.graphics.getWidth
-		local oldGetHeight = love.graphics.getHeight
-		function love.graphics.getWidth()
-			return oldGetWidth() / scale
-		end
-		function love.graphics.getHeight()
-			return oldGetHeight() / scale
-		end
+
+if scale == 1 then scale = nil end
+if scale then
+	local oldGetWidth  = love.graphics.getWidth
+	local oldGetHeight = love.graphics.getHeight
+	function love.graphics.getWidth()
+		return oldGetWidth() / scale
 	end
+	function love.graphics.getHeight()
+		return oldGetHeight() / scale
+	end
+end
 
 function flow.load()
 	print 'Welcome to Itemology!'
