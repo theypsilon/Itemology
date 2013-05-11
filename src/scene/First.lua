e9d163089dab3e9e379c30e5467fda903c8ce5dd
José manuel Barroso Galindo <theypsilon@gmail.com>
2013-05-11 05:09:14 +0200

    Cosmetics

--- src/scene/First.lua
+++ src/scene/First.lua
@@ -1,32 +1,33 @@
 local scene = {}
 
-local level, player, cameras = nil, nil, {}
 function scene.load()
 	require 'entity.Player'
 
-    level    = Level("plattform.tmx")
-    player   = Player(level, 100, 100)
-    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
-    cameras[Camera(player, {x=0,   y=0  , w=w/2, h=h/2},{x=40, y=40})] = true
-    cameras[Camera(player, {x=w/2, y=0  , w=w/2, h=h/2}             )] = true
-    cameras[Camera(player, {x=0  , y=h/2, w=w/2, h=h/2},{x=80, y=65})] = true
-    cameras[Camera(player, {x=w/2, y=h/2, w=w/2, h=h/2},{x=10, y=10})] = true
+    local level    = Level("plattform.tmx")
+    local player   = Player(level, 100, 100)
+    local cameras  = {}
+    local w, h     = love.graphics.getWidth(), love.graphics.getHeight()
+    cameras[Camera(player, {x=0,   y=0  , w=w/2, h=h/2}, {x=40, y=40})] = true
+    cameras[Camera(player, {x=w/2, y=0  , w=w/2, h=h/2}              )] = true
+    cameras[Camera(player, {x=0  , y=h/2, w=w/2, h=h/2}, {x=80, y=65})] = true
+    cameras[Camera(player, {x=w/2, y=h/2, w=w/2, h=h/2}, {x=10, y=10})] = true
+    scene.cameras, scene.level, scene.player = cameras, level, player
 end
 
 function scene.draw()
-    for camera,_ in pairs(cameras) do
+    for camera,_ in pairs(scene.cameras) do
         camera:draw()
     end
     love.graphics.print(
-        "tick " .. player._ticks .. 
-        "\nx: " .. player.x ..
-        "\ny: " .. player.y , 
+        "tick " .. scene.player._ticks .. 
+        "\nx: " .. scene.player.x ..
+        "\ny: " .. scene.player.y , 
         20, 20 )
 end
 
 function scene.update(dt)
     if pause then return end
-    level:tick(dt)
+    scene.level:tick(dt)
 end
 
 function scene.focus(inside)
