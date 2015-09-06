b8170321583cbd3579a32b6968f76fab46daf737
José manuel Barroso Galindo <theypsilon@gmail.com>
2015-09-06 23:13:54 +0700

    Moving components away

--- src/Camera.lua
+++ src/Camera.lua
@@ -2,36 +2,15 @@ local Layer, Graphics, Data; import()
 
 local Camera = class()
 
-local function validate(area, padding)
-    if area and (not area.x or not area.y or not area.w or not area.h) then
-        error 'incorrect area: not recognized {x,y,w,h}'
-    end
-    if padding and (not padding.x or not padding.y) then
-        error 'incorrect padding: not recognized {x,y}'
-    end
-end
-
 local z = 527
 
 function Camera:_init(target, area, padding)
-    validate(area, padding)
-
     self.cam = MOAICamera.new()
+    self:setTarget(target)
+    self:setPadding(padding)
+    self:setArea(area)
+
     local f = Data.MainConfig.screen.width / Data.MainConfig.world.width
-
-    self.padding = padding or { x = 0, y = 0 }
-
-    if target then
-        self:setTarget(target)
-        self.area    = area or { 
-            x = 0, y = 0, 
-            w = Graphics.getWidth () <  self._limit.w and
-                Graphics.getWidth () or self._limit.w, 
-            h = Graphics.getHeight() <  self._limit.h and
-                Graphics.getHeight() or self._limit.h,
-        }
-    end
-
     self.z = f * z
 end
 
@@ -39,6 +18,26 @@ local function validateTarget(target)
     return target and target.pos and
         (target.pos.x or target.x) and
         (target.pos.y or target.y)
+end
+
+function Camera:setPadding(padding)
+    if padding and (not padding.x or not padding.y) then
+        error 'incorrect padding: not recognized {x,y}'
+    end
+    self.padding = padding or { x = 0, y = 0 }
+end
+
+function Camera:setArea(area)
+    if area and (not area.x or not area.y or not area.w or not area.h) then
+        error 'incorrect area: not recognized {x,y,w,h}'
+    end
+    self.area    = area or { 
+        x = 0, y = 0, 
+        w = Graphics.getWidth () <  self._limit.w and
+            Graphics.getWidth () or self._limit.w, 
+        h = Graphics.getHeight() <  self._limit.h and
+            Graphics.getHeight() or self._limit.h,
+    }
 end
 
 function Camera:setTarget(target)
